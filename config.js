7a618a55f825c17d83f346432ecf6435ab555464
rapidcoderx <nitishvsrm@gmail.com>
2025-06-25 10:48:49 -0600

    prefix update

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: `/personal/`, // This path is subpath of your hosting https://domain/portfolio
+  pathPrefix: `/`, // This path is subpath of your hosting https://domain/portfolio
   firstName: 'Sathishkumar ',
   lastName: 'Krishnan',
   // social
